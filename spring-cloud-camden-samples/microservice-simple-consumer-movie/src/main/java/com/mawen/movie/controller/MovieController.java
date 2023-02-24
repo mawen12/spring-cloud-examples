cc10c7b405c4999bc5539c1f811ec6552d1fb3f8
mawen12 <1181963012mw@gmail.com>
2023-02-24 20:04:01 +0800

    Eureka

--- spring-cloud-camden-samples/microservice-simple-consumer-movie/src/main/java/com/mawen/movie/controller/MovieController.java
+++ spring-cloud-camden-samples/microservice-simple-consumer-movie/src/main/java/com/mawen/movie/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.mawen.movie.controller;
 
 import com.mawen.movie.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,16 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MovieController {
 
+    @Value("${user.userServiceUrl}")
+    private String userServiceUrl;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable long id) {
-        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+//        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class); // 硬编码的方式
+        return this.restTemplate.getForObject(this.userServiceUrl + id, User.class); // 读取配置文件
     }
 
 }
