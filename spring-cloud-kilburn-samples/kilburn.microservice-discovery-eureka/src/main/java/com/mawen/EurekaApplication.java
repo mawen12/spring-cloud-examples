e5f1cc0c06cbca6a758567e757194ef4c6f4e4a7
mawen12 <1181963012mw@gmail.com>
2023-03-07 23:31:06 +0800

    spring cloud hoxton 问题修复

--- spring-cloud-kilburn-samples/kilburn.microservice-discovery-eureka/src/main/java/com/mawen/EurekaApplication.java
+++ spring-cloud-kilburn-samples/kilburn.microservice-discovery-eureka/src/main/java/com/mawen/EurekaApplication.java
@@ -2,17 +2,18 @@ package com.mawen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author <a href="1181963012mw@gmail.com">mawen12</a>
  * @since 2023/3/7
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-public class UserApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
+
 }
