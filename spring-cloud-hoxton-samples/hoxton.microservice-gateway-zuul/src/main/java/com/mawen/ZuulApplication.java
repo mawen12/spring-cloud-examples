da5b046706dd54b05cc0e23c4a8b65ba4cfafd4c
mawen12 <1181963012mw@gmail.com>
2023-03-09 14:50:55 +0800

    spring cloud hoxton gateway zuul

---
+++ spring-cloud-hoxton-samples/hoxton.microservice-gateway-zuul/src/main/java/com/mawen/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.mawen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author <a href="1181963012mw@gmail.com">mawen12</a>
+ * @since 2023/3/9
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-hoxton-samples/hoxton.microservice-gateway/src/main/java/com/mawen/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.mawen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author <a href="1181963012mw@gmail.com">mawen12</a>
+ * @since 2023/3/9
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
